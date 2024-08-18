706c4fd1e831358e795130e0e33e432f010c3a35
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-08-18 21:06:39 +0400

    Update head3.vue

--- src/components/head3.vue
+++ src/components/head3.vue
@@ -2,6 +2,7 @@
     <div>
         <div class="box">
             <h1>ალგანი დენტი</h1>
+            <button>კითხვა გაქვთ? დაგვიკავშირდით</button>
         </div>
         
     </div>
@@ -21,6 +22,14 @@ export default {
 h1{
     color: rgb(0,101,237);
     font-weight: 900;
+    margin-left: 190px;
+    transform: translateY(170px);
+}
+
+button{
+    margin-left: 190px;
+    transform: translateY(175px);
+
 }
     
 </style>
