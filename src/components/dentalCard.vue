48068c813af6efa845fd2e08ed976375bacd69aa
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-08-21 18:14:18 +0400

    Add card components through the data.json file (data.json is imaginary backend)

--- src/components/dentalCard.vue
+++ src/components/dentalCard.vue
@@ -11,20 +11,38 @@ export default {
 </script>
 
 <template>
-  <div class="dentalCard">
-    <h1>{{ nivti.name }}</h1>
-    <p>{{ nivti.profession }}</p>
-  </div>        
+        <center><div class="boX">
+            <div class="dentalCard">
+                <center><img src="../assets/noimage.png" alt="errors" height="200" width="280"></center>
+                <center><h1>{{ nivti.name }}</h1></center>
+                <center><h2>{{ nivti.profession }}</h2></center>
+            </div>  
+        </div></center>     
 </template>
 
 <style scoped>
 .dentalCard {
-  height: 330px;
-  width: 270px;
-  background-color: red;
+    height: 330px;
+    width: 290px;
+    background-color: rgb(236,244,255);
+    font-size: 17px;
+    margin-left: -600px;
+    border-radius: 10px;
+    border: 1px solid blue;
+    color: rgb(0,101,237);
 }
 
-h1 {
-  color: red;
+img{
+    border-radius: 10px;
 }
+
+h1{
+    font-size: 20px;
+    font-weight: 700;
+}
+
+h2{
+    font-size: 21px;
+}
+
 </style>
--- src/components/dentists.vue
+++ src/components/dentists.vue
@@ -19,10 +19,11 @@ export default {
 <template>
     <div>
       <div class="bigBox">
-        <center><h2>ჩვენი სტომატოლოგები</h2></center>
+        <center><h2 class="txt">ჩვენი მიღწევები</h2></center>
         <div class="card">
-          <dentalCard v-for="(item, index) in dental1" :key="item.id" :nivti="item"/>
+          <dentalCard v-for="(item, index) in dental1" :key="item.id" :nivti="item" class="cards"/>
         </div>
+        <br/><br/>
       </div>
     </div>
   </template>
@@ -31,5 +32,24 @@ export default {
 <style scoped>
 .bigBox{
     background-color: rgb(180,212,255);
+    border: 1px solid blue;
+}
+
+.card{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-left: 550px;
+}
+
+.cards{
+    flex: 0 0 calc(33.33% - 10px); /* 33.33% width minus spacing */
+    margin-top: 20px;
+}
+
+.txt{
+    font-size: 30px;
+    font-weight: 700;
+    color: rgb(0,101,237);
 }
 </style>
