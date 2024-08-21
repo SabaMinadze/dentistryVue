966620465ed0bd8b9630646291b53df13fd121e6
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-08-21 17:36:59 +0400

    Add vue files

--- src/components/dentalCard.vue
+++ src/components/dentalCard.vue
@@ -1,37 +1,30 @@
 <script>
 export default {
-  name: 'App',
-  data (){
-        return{
-            data1: data,
-            
-        }
-    },
-    props: {
-        nivti: {
-            required: true,
-            type: Object,
-        }
+  name: 'dentalCard',
+  props: {
+    nivti: {
+      required: true,
+      type: Object,
     }
+  }
 };
 </script>
 
 <template>
-    <div>
-        <div class="dentalCard">
-            <h1>{{ nivti.name }}</h1>
-            <h1>Hello</h1>
-            
-        </div>        
-    </div>
+  <div class="dentalCard">
+    <h1>{{ nivti.name }}</h1>
+    <p>{{ nivti.profession }}</p>
+  </div>        
 </template>
 
-
 <style scoped>
-.dentalCard{
-    height: 330px;
-    width: 270px;
-    background-color: red;
+.dentalCard {
+  height: 330px;
+  width: 270px;
+  background-color: red;
 }
-    
+
+h1 {
+  color: red;
+}
 </style>
--- src/components/dentists.vue
+++ src/components/dentists.vue
@@ -1,6 +1,7 @@
 <script>
-import dentalCard from "../components/dentalCard.vue"
-import data from '../data.json'
+import dentalCard from "../components/dentalCard.vue";
+import data from '../data.json';
+
 export default {
   name: 'App',
   components: {
@@ -8,44 +9,27 @@ export default {
   },
   data() {
     return {
-        data: data,
+      dental1: data,
     }
-  },
+  }
 };
 </script>
 
+
 <template>
     <div>
-        <div class="bigBox">
-            <center><h2>ჩვენი სტომატოლოგები</h2></center>
-            <div class="card">
-                <dentalCard v-for="(item, index) in data " :key="index" :nivti="item"/>
-            </div>
-        </div>    
+      <div class="bigBox">
+        <center><h2>ჩვენი სტომატოლოგები</h2></center>
+        <div class="card">
+          <dentalCard v-for="(item, index) in dental1" :key="item.id" :nivti="item"/>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
 
 
 <style scoped>
 .bigBox{
-    height: 800px;
-    width: 100%;
-    background-color: rgb(180,212,255);
-    border: 1px solid blue;
-    transform: translateY(-50px)
-
+    background-color: blue;
 }
-
-.card{
-    margin-left: 70px;
-    transform: translateY(20px)
-}
-
-h2{
-    font-size: 30px;
-    font-weight: 700;
-    color: rgb(0,101,237);
-    transform: translateY(20px);
-}
-    
 </style>
